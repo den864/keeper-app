8c1f4768bbcdb484058c1ced097e9daa51f4b335
den864 <nsnszjz@gmail.com>
2021-10-16 16:13:40 +0530

    build added

---
+++ build/precache-manifest.cddff44e3fdfc4f71e8aa628757bb1e7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31625dd0ca4fc63234a005444ede6e3",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "4b184f0b9c47ebc07f24",
+    "url": "/keeper-app/static/js/2.865dfa49.chunk.js"
+  },
+  {
+    "revision": "386b22198f177b626e17",
+    "url": "/keeper-app/static/js/main.353f869a.chunk.js"
+  },
+  {
+    "revision": "fb7d45a05bf0dfe0fe92",
+    "url": "/keeper-app/static/js/runtime-main.42e7a143.js"
+  }
+]);
